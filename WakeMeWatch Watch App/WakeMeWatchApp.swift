e6531bfd3c40aad48a52a9f514b2dde74372bd6a
Juhi Damley <juhidamley@hotmail.com>
2026-02-02 02:07:38 -0800

    Create Setup

---
+++ WakeMeWatch Watch App/WakeMeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeMeWatchApp.swift
+//  WakeMeWatch Watch App
+//
+//  Created by Juhi Damley on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeMeWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
