dc55af966f97c20d11a26d17f866a43596484856
Juhi Damley <juhidamley@hotmail.com>
2026-02-02 02:01:44 -0800

    Initial Commit

---
+++ WakeMe/WakeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeMeApp.swift
+//  WakeMe
+//
+//  Created by Juhi Damley on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
